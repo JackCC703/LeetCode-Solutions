fef42a8998aa6859ed65def96d5fe241c23fc959
Jack CC <chencan0703@gmail.com>
2026-03-29 14:36:20 +0800

    Time: 3 ms (68.93%), Space: 64.4 MB (96.12%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.ts
@@ -0,0 +1,27 @@
+function lemonadeChange(bills: number[]): boolean {
+    let five = 0;
+    let ten = 0;
+
+    for (let i = 0; i < bills.length; i++) {
+        if (bills[i] === 5) {
+            five++;
+        }
+        if(bills[i] === 10) {
+            five--;
+            ten++;
+        }
+        if (bills[i] === 20) {
+            if (ten > 0) {
+                ten--;
+                five--;
+            }
+            else {
+                five -= 3;
+            }
+        }
+        if (five < 0 || ten < 0) {
+            return false;
+        }
+    }
+    return true;
+};
