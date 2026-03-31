1dde2afb0de4b42b3ee27590b40e768864cfa39f
Jack CC <chencan0703@gmail.com>
2026-03-31 11:01:28 +0800

    Time: 27 ms (39.22%), Space: 58.6 MB (88.24%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.ts
@@ -0,0 +1,12 @@
+function largestPerimeter(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    for (let i = nums.length - 1; i >= 2; i--) {
+        let a = nums[i - 2];
+        let b = nums[i - 1];
+        let c = nums[i];
+        if(a + b > c) {
+            return a + b + c;
+        }
+    }
+    return 0;
+};
